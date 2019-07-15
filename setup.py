5ac25f843ed943f3d486e8552cea1dd3fab912b8
Gaurav <shimpigaurav0@gmail.com>
2019-07-15 14:39:22 +0530

    Version Updates

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="pandasUtilities",
-    version="0.0.1",
+    version="0.0.2",
     license='MIT',
     author="Gaurav Shimpi",
     author_email="shimpigaurav0@gmail.com",
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    download_url = 'https://github.com/gauravshimpi/PandasUtilities/archive/v_0_0_1.tar.gz',
+    download_url = 'https://github.com/gauravshimpi/PandasUtilities/archive/V_0_0_2.tar.gz',
     keywords = ['pandas', 'utilities', 'PandasUtilities'],
     install_requires=setuptools.find_packages(),
 )
